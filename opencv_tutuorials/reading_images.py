8058dabd10d6ee48f1f8029afad6a3902de9546d
Malaki <michael.malaki@tescan.com>
2025-10-03 15:12:25 +0200

    v

--- opencv_tutuorials/reading_images.py
+++ opencv_tutuorials/reading_images.py
@@ -2,18 +2,21 @@ import cv2 as cv
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from skimage import io
 
 
 def read_image():
     root = os.getcwd()
-    imgPath = os.path.join(root,"images","peppers_color.tif")
+    imgPath = os.path.join(root,"images","lena_color_256.tif")
+    print(f"Current directory: {root}")
     print(f"Image apth: {imgPath}")
-    img = cv.imread(imgPath)
+    img = io.imread(imgPath)
     if img == None:
         print("Image Not Found")
         return
-    cv.imshow("img", img)
-    cv.waitKey(0) # 0 means infinity
+    io.imshow("img", img)
+    plt.show()
+    #cv.waitKey(0) # 0 means infinity
 
 
 if __name__ == "__main__":
